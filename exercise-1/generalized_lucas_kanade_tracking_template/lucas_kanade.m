1f715ab9cfee201c3dfcb71ee7c524bd1fccbaef
Duke Nguyen <duke7295@gmail.com>
2020-11-16 22:19:06 +0100

    Update lucas_kanade.m

--- exercise-1/generalized_lucas_kanade_tracking_template/lucas_kanade.m
+++ exercise-1/generalized_lucas_kanade_tracking_template/lucas_kanade.m
@@ -6,5 +6,25 @@ function [u, v] = lucas_kanade(im1, im2, window_size)
   %   u, v:        Optical flow in u and v direction
 
   %% TODO
-
+  sigma = 1;
+  [imgDx, imgDy] = gaussderiv(im1, sigma);  
+  gaussian = gauss(1, sigma);
+  imgDt = im2 - im1;
+  imgDt = conv2(imgDt, gaussian , 'same');
+  imgDt = conv2(imgDt, gaussian', 'same');
+    
+  u = zeros(size(im1,1:2));
+  v = zeros(size(im1,1:2));
+ 
+  for i=1:size(im1,1)-window_size+1
+      for j=1:size(im1,2)-window_size+1
+          A = [reshape(imgDx(i:i+window_size-1, j:j+window_size-1), [window_size^2, 1]), ...
+               reshape(imgDy(i:i+window_size-1, j:j+window_size-1), [window_size^2, 1])];
+          b = reshape(imgDt(i:i+window_size-1, j:j+window_size-1), [window_size^2, 1]);
+          d = (A'*A)\A'*b;
+          u(i+floor(window_size/2),j+floor(window_size/2)) = d(1);
+          v(i+floor(window_size/2),j+floor(window_size/2)) = d(2);
+      end
+  end
+  
 end
